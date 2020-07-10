6d940241d51899a78a329272a1bd16a1a56fb7d4
botfinski <botfinski@gmail.com>
2020-07-10 04:27:14 +0200

    json

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
 import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
 import json from './data.json'
 
 function App() {
-	const [data, setData] = useState({ hits: [] });
-
-	useEffect(() => {
-		const fetchData = async () => {
-			// const result = await axios(
-			// 	'https://hn.algolia.com/api/v1/search?query=redux',
-			// );
-
-			// const result = await axios(data);
-
-			// console.log(result)
-			// console.log(json)
-
-			setData(json);
-		};
-
-		fetchData();
-	}, []);
+	// console.log(json)
 
 	return (
-		<ul>
-			{data.hits.map(item => (
-				<li key={item.objectID}>
-					<a href={item.url}>{item.title}</a>
-				</li>
-			))}
-		</ul>
+		<>
+			{
+				json.map(army => {
+
+					for (const prop in army) {
+						// console.log(prop)
+						console.log(army[prop])
+
+						return (
+							<p>{army[prop].name}</p>
+						)
+					}
+				})
+			}
+		</>
 	);
 }
 
