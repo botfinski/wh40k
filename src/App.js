a1d45164c2df23ea5cadd9512a6eac26f2fff3e6
botfinski <botfinski@gmail.com>
2020-07-11 19:14:58 +0200

    aaaaaaaaaa

--- src/App.js
+++ src/App.js
@@ -2,23 +2,50 @@ import React, { useState, useEffect } from 'react';
 import json from './data.json'
 
 function App() {
-	// console.log(json)
+
+	const [data] = useState(json);
+	// console.log(data)
+
+	const Unit = ({ unit }) => {
+		for (const prop in unit) {
+			console.log(typeof unit)
+			return (
+				<p key={unit}>unit</p>
+			)
+		}
+	}
+
+	const Units = ({ units }) => {
+		for (const prop in units) {
+			// console.log(units[prop])
+			return (
+				<ul>
+					<li>a</li>
+				</ul>
+			)
+		}
+	}
+
+	const army = (
+		data.map((army, i) => {
+			for (const prop in army) {
+				// console.log(army[prop])
+				return (
+					<div key={i}>
+						<p>{army[prop].name}</p>
+						<div>Units</div>
+						<Units unit={army[prop].units} />
+					</div>
+				)
+			}
+		})
+	);
+
+
 
 	return (
 		<>
-			{
-				json.map(army => {
-
-					for (const prop in army) {
-						// console.log(prop)
-						console.log(army[prop])
-
-						return (
-							<p>{army[prop].name}</p>
-						)
-					}
-				})
-			}
+			{army}
 		</>
 	);
 }
